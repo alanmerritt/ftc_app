d78dc0770046329251e42097729ad27eb5103ca1
alanmerritt <merrittalan1@gmail.com>
2018-11-16 15:29:39 -0700

    Created opMode.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous_Teaching_Test.java
@@ -0,0 +1,26 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+/**
+ * Created by MerrittAM on 11/8/2018.
+ */
+
+public class Autonomous_Teaching_Test extends OpMode {
+	
+	Robot robot;
+	
+	@Override
+	public void init() {
+		
+		robot = new Robot(this);
+		
+	}
+	
+	@Override
+	public void loop() {
+	
+	
+	
+	}
+}
