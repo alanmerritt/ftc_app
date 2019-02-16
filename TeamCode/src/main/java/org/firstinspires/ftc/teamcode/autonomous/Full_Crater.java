5e641a7cf7555001ba9e96e1596fb361049ba64f
WillettTK <willetttk@pcsd1.org>
2019-02-16 07:18:39 -0700

    Fixed autonomous better and stuff.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/Full_Crater.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/Full_Crater.java
@@ -13,21 +13,26 @@ public class Full_Crater extends Auto {
 	public void runOpMode() {
 	
 	initialize();
-	
+
+	activateElementDetection();
+
+	telemetry.addLine("Ready to go!");
+	telemetry.update();
+
 	waitForStart();
 	
 	//rest of this line is for unhooking
 	lowerBot();
-	   sleep(200);
+	   sleep(100);
 	
 	driveLeft(6,1,this);
-	   sleep(200);
+	   sleep(100);
 	
 	driveForward(3,1,this);
-	   sleep(200);
+	   sleep(100);
 
 	   driveRight(6,1,this);
-	      sleep(200);
+	      sleep(100);
 
 
 
@@ -43,7 +48,7 @@ public class Full_Crater extends Auto {
 
 	robot.markerDropperretract();
 		//Start detecting the elements.
-	activateElementDetection();
+//	activateElementDetection();
 	
 	//Detect the elements. Save the position (LEFT, CENTER,
 	//RIGHT, or UNKNOWN) into the position variable.
@@ -70,7 +75,7 @@ public class Full_Crater extends Auto {
 	else
 	{
 
-		telemetry.addLine("Sherman you broke me i have a wife and kids how could you do this(Warning!!! position could not be determined)!!!");
+		telemetry.addLine("This is Shermans fault(Warning!!! position could not be determined)!!!");
 		telemetry.update();
 
 		knockOffCenter();
@@ -81,120 +86,77 @@ public class Full_Crater extends Auto {
 		
 //		knockoffright();
 		
-		   sleep(200);
+		rotateCCW(50,1);
+		   sleep(100);
 		
-		rotateCCW(45,1);
-		   sleep(200);
-		
-		driveLeft(3,1,this);
-		   sleep(200);
+		driveLeft(7,1,this);
+		   sleep(100);
 
-		driveForward(25,1,this);
-		   sleep(200);
+		driveForward(35,1,this);
+		   sleep(100);
 		
-		rotateCCW(100,1);
-		   sleep(200);
-		
-		driveRight(35,.7,this);
-		//robot is straighting its self out on the wall
-		   sleep(200);
-		
-		driveLeft(8,1,this);
-		//drives left so it doesnt get stuck on the wall
-		   sleep(200);
-		
-		driveForward(55,1,this);
-		   sleep(200);
-		
-		rotateCW(180,1);
-		   sleep(200);
+		rotateCCW(120,1);
+		   sleep(100);
 
-		   driveBackward(5,1,this);
-		      sleep(200);
+		   driveRight(5,1,this);
+		      sleep(100);
+
+
+		      driveForward(55,1,this);
+		   sleep(100);
 
 		   robot.markerDropperdeposit();
-		//drops the team marker
-		//in case the team marker gets stuck on the dropper it shakes it off
-		driveBackward(10,1,this);
-		   sleep(100);
-		
-		driveForward(5,1,this);
 		   sleep(100);
 
-		driveBackward(5,1,this);
-		   sleep(100);
-		
-		driveForward(5,1,this);
-		   sleep(100);
-		//ending the shake off
-
-		driveBackward(30,1,this);
-		   sleep(200);
-		
-		rotateCCW(180,1);
-		   sleep(200);
-
-		   driveLeft(4,1,this);
-		//puts it back up against the wall
-		   sleep(200);
-
-		driveRight(2,.3,this);
-		   sleep(200);
-
-		driveForward(20,1,this);
-		   sleep(200);
-		
-		driveForward(15,.4,this);
-		   sleep(200);
+		driveBackward(70,1,this);
 	}
 	
 	void knockOffCenter(){//justs drives forwards and backwards
 	
-		driveForward(25,.5,this);
-		   sleep(200);
+		driveForward(30,.5,this);
+		   sleep(100);
 		
-	driveBackward(15,.5,this);
+	driveBackward(20,.5,this);
 	}
 
 	void knockOffRight()
 	{
-	    //justs drives forward and knocks of the middle\
-		// 	driveForward(5,1,this);
-	    sleep(200);
-	    
-		driveForward(2,.5,this);
-	       sleep(200);
+		driveForward(15,.5,this);
+	       sleep(100);
 
 	    driveRight(15,1,this);
-		   sleep(200);
+		   sleep(100);
 	    
 	    driveForward(20,.5,this);
-	       sleep(200);
+	       sleep(100);
 	    
 	    driveBackward(15,.5,this);
-	       sleep(200);
+	       sleep(100);
 	    
 	    driveLeft(15,1,this);
-	       sleep(200);
+	       sleep(100);
+
+	    driveBackward(5,1,this);
+	       sleep(100);
 	}
 
 	void knockOffLeft()//make sure it doesnt hit the landers leg
 	{
 
-	driveForward(5,1,this);
-	   sleep(200);
+	driveForward(10,1,this);
+	   sleep(100);
 
-	driveLeft(15,1,this);
-	   sleep(200);
+	driveLeft(10,.7,this);
+	   sleep(100);
 
-	driveForward(20,1,this);
-	   sleep(200);
+	driveForward(30,1,this);
+	   sleep(100);
 
 	driveBackward(20,1,this);
-	   sleep(200);
+	   sleep(100);
 
 	driveRight(15,1,this);
-	   sleep(200);
+	   sleep(100);
 	}
 	//squiggle is the end of the entire code
 
